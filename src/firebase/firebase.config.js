66dd4bdcb9c201416803fb5c29591c85d46f598f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-16 23:23:37 +0600

    update index.html title and favicon; modify firebase config for new environment variables; enhance tailwind config with new color settings and sidebar styles

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_AUTH_API,
-  authDomain:import.meta.env.VITE_AUTH_AUTHDOMAIN,
-  projectId:import.meta.env.VITE_AUTH_PROJECT,
-  storageBucket:import.meta.env.VITE_AUTH_STORAGEBUCKET,
-  messagingSenderId:import.meta.env.VITE_AUTH_MESSAGINGSENDER,
-  appId:import.meta.env.VITE_AUTH_APP
+  apiKey:import.meta.env.VITE_FIRE_API,
+  authDomain:import.meta.env.VITE_FIRE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_FIRE_PROJECT,
+  storageBucket:import.meta.env.VITE_FIRE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_FIRE_MESSAGINGSENDER,
+  appId:import.meta.env.VITE_FIRE_APP
 };
 
 // Initialize Firebase
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ export default {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		colors: {
-  			background: 'hsl(var(--background))',
+  			background: '#000000',
+  			background2: '#222222',
+  			baseColor: '#d4ff00',
   			foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
@@ -49,6 +51,16 @@ export default {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		}
   	}
