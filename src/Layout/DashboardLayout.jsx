119b585c589b74d45a0cba79b50026d79a494597
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-12 22:59:03 +0600

    refactor DashboardLayout component for improved readability and structure

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -3,25 +3,27 @@ import DashboardSidebar from "@/components/DashboardView/common/DashboardSidebar
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
-
 const DashboardLayout = () => {
   const [profileOpen, setProfileOpen] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <div className="relative pt-[70px] h-screen scroll-smooth">
-      
-     <DashboardHeader profileOpen={profileOpen} setProfileOpen={setProfileOpen}/>
+      <DashboardHeader
+        profileOpen={profileOpen}
+        setProfileOpen={setProfileOpen}
+      />
 
-      <div>
-        <div className="flex items-start">
-          <DashboardSidebar setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen}/>
+Hello senders
+      <div className="flex items-start">
+        <DashboardSidebar
+          setSidebarOpen={setSidebarOpen}
+          sidebarOpen={sidebarOpen}
+        />
 
-          <section className="main-content w-full overflow-auto p-6 ">
-              <Outlet/>
-            {/* <div className="overflow-x-auto no-scrollbar">
-            </div> */}
-          </section>
-        </div>
+        <section className="main-content w-full overflow-auto p-6 ">
+          <Outlet />
+          
+        </section>
       </div>
     </div>
   );
